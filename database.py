8f836c2f46098fa17ebd9abf81efe5018025753a
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2023-02-27 23:22:02 +0530

    Chenage

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 # print(sqlalchemy.__version__)
-db_con = os.environ['DB_CONNECTION']
+db_con = "mysql+pymysql://34adkx5cxuz5izr49wms:pscale_pw_JKYb1ivzj8kMxxNyP8whBaDBKbBJpwb9HPslekRU695@aws.connect.psdb.cloud/bhai?charset=utf8mb4"
 engine = create_engine(db_con,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"

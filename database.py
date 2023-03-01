8679b810f14cc74cfe9203419faf1fbd59e484e1
1P1 <124462509+microsoftwala@users.noreply.github.com>
2023-03-01 07:06:09 +0000

    change

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
-import os
-# print(sqlalchemy.__version__)
-db_con = "mysql+pymysql://34adkx5cxuz5izr49wms:pscale_pw_JKYb1ivzj8kMxxNyP8whBaDBKbBJpwb9HPslekRU695@aws.connect.psdb.cloud/bhai?charset=utf8mb4"
+
+db_con="mysql+pymysql://34adkx5cxuz5izr49wms:pscale_pw_JKYb1ivzj8kMxxNyP8whBaDBKbBJpwb9HPslekRU695@aws.connect.psdb.cloud/bhai?charset=utf8mb4"
+# db_con = os.environ['DB_CONNECTION']
 engine = create_engine(db_con,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
@@ -15,3 +15,28 @@ def load_jobs_from_db():
     for row in result.all():
       my_dict.append(row)
     return my_dict
+
+def load_job_from_db():
+  with engine.connect() as connection:
+    result = connection.execute(text("select * from jobs"))
+    my_dict = []
+    for row in result.all():
+      my_dict.append(row)
+  
+    return my_dict
+
+def add_application_to_db(job_id,data):
+  with engine.connect() as connection:
+    # query = text("INSERT INTO application (job_id,full_name,email,linkedin_url,education_url,work_experiance,resume_url) VALUES (%d,%s,%s,%s,%s,%s,%s)")
+
+    # connection.execute(query,job_id,
+    #              data['fullname'],
+    #              data['email'],
+    #              data['emailss'],
+    #              data['education'],
+    #              data['experiance'],
+    #              data['emails'])
+
+    t = text("SELECT * FROM jobs WHERE id =:user_id")
+    result = connection.execute(t, user_id=2)
+    return result
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-from flask import Flask,render_template,jsonify
-from database import load_jobs_from_db
-
+from flask import Flask, render_template,request
+from database import load_jobs_from_db, load_job_from_db,add_application_to_db
 
 app = Flask(__name__)
 
-  
+
 @app.route("/")
 def hello_world():
-  Jobs=load_jobs_from_db()
-  return render_template ('home.html',jobs=Jobs)
+  Jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=Jobs)
 
 
 
-@app.route("/api/jobs")
-def jobs():
-  jobs=load_jobs_from_db()
-  return jsonify(jobs)
-  
+
+@app.route("/job/<id>")
+def jobs_data(id):
+  Jobs = load_job_from_db()
+  return render_template('jobpage.html', jobs=Jobs,id=int(id))
+
+
+
+@app.route("/job/<id>/apply",methods=['post','get'])
+def apply_for_job(id):
+  data = request.form
+  Jobs = load_job_from_db()
+  add_application_to_db(int(id),data)
+  return render_template('application_submitted.html',application=data,jobs=Jobs)
+
+
+              
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True) 
+  app.run(host='0.0.0.0', debug=True)
